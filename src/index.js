971b1c6dd41e3d68513e23c2b75c7c5781df1a1f
fsarachu <fsarachu@gmail.com>
2017-06-17 23:37:17 -0300

    Add comment to rest parameters function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-// Rest parameters
+/**
+ * ---- Rest Parameters ----
+ * Only the last parameter of a function may be marked as a rest parameter.
+ * In a call, the parameters before the rest parameter are filled as usual.
+ * Any “extra” arguments are put into an array and assigned to the rest parameter.
+ * If there are no extra arguments, the rest parameter will simply be an empty array;
+ * the rest parameter will never be undefined.
+ */
 function containsAll(haystack, ...needles) {
     for (let needle of needles) {
         if (haystack.indexOf(needle) === -1) {
